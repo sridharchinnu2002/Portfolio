0aeaac0185d3e70879918dd0435578a6a6091dad
sridharchinnu2002 <chinthalasrishar70@gmail.com>
2025-03-13 12:17:45 +0530

    Updated code

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
-      <h3>this is contact page</h3>
+    <div className='flex justify-center bg-black p-30'>
+      <img src="https://www.kindpng.com/picc/m/270-2708235_new-content-coming-soon-web-page-is-under.png" alt=""  height={300} width={400}/>
     </div>
   )
 }
 
 export default Contact
+git 
