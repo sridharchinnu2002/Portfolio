dd0f83bb8995a6fcb7ce6dce8eba1865f2d28c8b
sridharchinnu2002 <chinthalasrishar70@gmail.com>
2025-03-14 03:33:19 +0530

    Updated portfolio project

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,12 +1,122 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Contact() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  })
+  const [errors, setErrors] = useState({})
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let newErrors = {}
+    const numberRegex = /^([0-9]{1,})$/
+    if (formData.name.trim() == "") {
+      newErrors.name = "Name is required"
+    }
+    if (formData.email.trim() == "") {
+      newErrors.email = "Email is required"
+    }
+    if (formData.number.trim() == "") {
+      newErrors.number = "Ph.No is required"
+    }
+    if (formData.message.trim() == "") {
+      newErrors.message = "message is required"
+    }
+    if (!numberRegex.test(formData.number)) {
+      newErrors.numberregex =
+        "Ph.No must contain Numbers from 0-9 only"
+    }
+    setErrors(newErrors)
+    if (setErrors == {}) {
+      fetch()
+    }
+  }
   return (
-    <div className='flex justify-center bg-black p-30'>
-      <img src="https://www.kindpng.com/picc/m/270-2708235_new-content-coming-soon-web-page-is-under.png" alt=""  height={300} width={400}/>
+    <div>
+      <div className=' flex justify-center  bg-black text-white text-3xl font-extrabold p-5'>
+        <p>GET IN TOUCH</p>
+      </div>
+      <div className=' flex justify-center  bg-black text-white p-2'>
+        <p><b>📞 Phone :</b> +91 9014578450</p>
+      </div>
+
+      <div className=' flex justify-center  bg-black text-white'>
+        <p><b>📧 Email :</b> chinthalasridhar70@gmail.com</p>
+      </div>
+
+      <div className=' flex justify-center  bg-black text-white p-2'>
+        <p><b>📍 Address :</b> 404-Error, Java lane, Void city ,NullPointer Dist </p>
+      </div>
+
+
+      <div className='flex justify-center bg-black text-white'>
+
+        <form className='flex justify-center' onSubmit={handleSubmit}>
+
+          <div>
+          <div className='flex justify-center m-3 pt-3'>
+                <div>
+                 <input
+                type="text"
+                name='name'
+                onChange={handleChange}
+                value={formData.name}
+                placeholder='Enter your name'
+                className='border-2 border-yellow-300 m-2 rounded-2xl bg-amber-50 text-black p-1'
+              />
+              <p className=' text-red-500'> {errors.name}</p>
+
+              <input
+                type="email"
+                name='email'
+                onChange={handleChange}
+                value={formData.email}
+                placeholder='Enter your EMail'
+                className='border-2 border-yellow-300 m-2 rounded-2xl bg-amber-50 text-black p-1'
+              />
+              <p className=' text-red-500'> {errors.email}</p>
+
+              <input
+                type="tel"
+                name='number'
+                onChange={handleChange}
+                value={formData.number}
+                placeholder='Enter your Ph.No'
+                className='border-3 border-yellow-300 m-2 rounded-2xl bg-amber-50 text-black p-1'
+              />
+              <p className=' text-red-500'> {errors.number}</p>
+              <p className=' text-red-500'> {errors.numberregex}</p>
+               </div>
+
+
+            <div>
+              <textarea
+                type="textarea"
+                name='message'
+                onChange={handleChange}
+                value={formData.message}
+                placeholder='Enter your message'
+                className='border-2 border-yellow-300 m-2 rounded-2xl bg-amber-50 text-black p-1 h-35'
+              />
+              <p className=' text-red-500'> {errors.message}</p>
+              </div>
+             
+            
+          </div>
+          <div className=' flex justify-center pb-5' > 
+            <button className='justify-center bg-yellow-300 text-black font-bold rounded-2xl h-10 w-50 m-3'>Submit</button></div>
+
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
 
 export default Contact
-git 
--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -1,63 +1,53 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+
 function Projects() {
   return (
-    <div className='bg-black text-white'>
-      <p className='text-3xl flex justify-center p-2'><b>My Technical Work</b></p>
-      <div class="flex ">
-          <div class="flex-1 flex justify-center border-2 bg-yellow-300 text-black ring-4 ring-black-500 m-3 mx-6 rounded-4xl shadow-lg shadow-red-400/50 p-2">
+    <div className='flex flex-col gap-2 bg-black text-white pt-8'>
+      <p className='flex justify-center  pb-6 text-3xl'><b>My Technical Work..!!!</b></p>
+      <div class=" border-2 text-yellow-300 bg-black ring-2 ring-black-500 m-3 mr-25 ml-25 pb-5 rounded-4xl shadow-lg shadow-red-400/50 ">
+      <b className='flex justify-center text-2xl p-3'><u>My_Hotel</u></b>
+            <div className='flex justify-evenly' >
+              <img className='rounded-4xl justify-center border-yellow-500 border-2' src=".\images\mh-1.png" alt="img" height={200} width={400} />
+           
+             <pre className='py-3'><i>✦ <b>Description:</b> A simple yet elegant food hotel<br />   website built using React.js, offering customers an interactive<br />   platform to explore dishes, view menus, and place orders. </i> <br />
+             <i>✦ Login options for(Customers) with high end security.</i><br />
             
-            <div >
-              <b className='flex justify-center'><u>Online Electronic Store</u></b>
-           
-             <pre className='py-3'><i>✦ Description: An online platform developed using  Java,<br />  JDBC, HTML,   and CSS,  allowing users to  browse and purchase<br />  electronic items securely. </i> <br />
-             <i>✦ GitHub Repository: <a href="https://github.com/sridharchinnu2002/Online_Electronic_Store"><u><b>Online_Electronic_Store</b></u></a></i></pre>
-             
-            </div>
-          </div>
-  
-        <div class="flex-1 flex justify-center border-2 bg-yellow-300 text-black ring-4 ring-black-500 m-3 mx-6 rounded-4xl shadow-lg shadow-red-400/50 p-2">
-            
-            <div >
-              <b className='flex justify-center'><u>Bank Registration System</u></b>
-           
-             <pre className='py-3'><i>✦ Description: A Java JEE-based system enabling <br />  authorized personnel to register,and manage bank employee details <br />  securely, integrating JSP, HTML, CSS, and SQL. </i> <br />
-              <i>✦ GitHub Repository: <a href="https://github.com/sridharchinnu2002/Bank_Registration"><u><b>Bank_Registration</b></u></a></i></pre>
-             
-            </div>
-          </div>
-        
-      </div>
-           
-      <div class="flex ">
-          <div class="flex-1 flex justify-center border-2 bg-yellow-300 text-black ring-4 ring-black-500 m-3 mx-6 rounded-4xl shadow-lg shadow-red-400/50 p-2">
-            
-            <div >
-              <b className='flex justify-center'><u>My_Hotel</u></b>
-           
-             <pre className='py-3'><i>✦ Description: A simple yet elegant food hotel <br />  website built using React.js, offering customers an interactive<br />  platform to explore dishes, view menus, and place orders. </i> <br />
+             <i>✦ Customers can Add/remove items before checkout. <br />  </i><br />
              <i>✦ GitHub Repository: <a href="https://github.com/sridharchinnu2002/myhotel"><u><b>myhotel</b></u></a></i></pre>
-             
             </div>
           </div>
-  
-        <div class="flex-1 flex justify-center border-2 bg-yellow-300 text-black ring-4 ring-black-500 m-3 mx-6 rounded-4xl shadow-lg shadow-red-400/50 p-2">
-            
-            <div >
-              <b className='flex justify-center'><u>Portfolio</u></b>
+          <div class=" border-2 bg-yellow-300 text-black ring-4 ring-black-500 m-3 mr-25 ml-25 pb-5 rounded-4xl shadow-lg shadow-red-400/50 ">
+             <b className='flex justify-center text-2xl pb-3'><u>Online Electronic Store</u></b>
+            <div className='flex justify-evenly' >
+              <img className='rounded-4xl justify-center' src=".\images\es-4.jpeg" alt="img" height={400} width={400} />
            
-             <pre className='py-3'><i>✦ Description: A personal portfolio website built from scratch <br />  using React.js and different libraries showcasing my <br />  skills, projects, and contact details.</i> <br />
-            <i>✦ GitHub Repository: <a href="https://github.com/sridharchinnu2002/Bank_Registration"><u><b>Portfolio</b></u></a></i></pre>
-             
+             <pre className='py-3'><i>✦ <b>Description:</b> An online platform developed using  Java,<br />  JDBC, HTML,   and CSS,  allowing users to  browse and purchase<br />  electronic items securely. </i> <br />
+             <i>✦ with 2 login options for(ADMIN & USER) with high end security.</i><br />
+             <i>✦ Admin can Manage users, orders, and product inventory..</i><br />
+             <i>✦ Customers can add products to the cart and proceed. <br />  </i><br />
+             <i>✦ GitHub Repository: <a href="https://github.com/sridharchinnu2002/Online_Electronic_Store"><u><b>Online_Electronic_Store</b></u></a></i></pre>
             </div>
           </div>
-        
-      </div>
 
-        <div className='text-3xl flex justify-center gap-5 p-2'>
+          <div class=" border-2 text-yellow-300 bg-black ring-2 ring-black-500 m-3 mr-25 ml-25 pb-5 rounded-4xl shadow-lg shadow-red-400/50 ">
+      <b className='flex justify-center text-2xl pb-3'><u>Bank Registration System</u></b>
+            <div className='flex justify-evenly' >
+              <img className='rounded-4xl justify-center border-yellow-500 border-2' src="https://s3-alpha.figma.com/hub/file/4397175417/07f7e307-881f-453a-ae3a-432059611638-cover.png" alt="img" height={200} width={300} />
+           
+             <pre className='py-3'><i>✦ <b>Description:</b>  A Java JEE-based system enabling<br />  authorized person to register,and manage bank employee details <br />  securely, integrating JSP, HTML, CSS, and SQL. </i> <br />
+             <i>✦ with 2 login options for(ADMIN & USER) with high end security.</i><br />
+             <i>✦ Admin can edit and remove customer records.</i><br />
+             <i></i><br />
+             <i>✦ GitHub Repository: <a href="https://github.com/sridharchinnu2002/Bank_Registration"><u><b>Bank Registration System</b></u></a></i></pre>
+            </div>
+          </div>
+          <div className='text-3xl flex justify-center gap-5 p-2'>
         <span ><b>For more projects visit </b></span>   
-        <SocialIcon url='https://github.com/sridharchinnu2002' /> </div>     
+        <SocialIcon url='https://github.com/sridharchinnu2002' /> </div> 
+
     </div>
+
   )
 }
 
